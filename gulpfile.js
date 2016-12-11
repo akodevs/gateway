ffef013fdcefc82c5670540f8dd1b7296ac72bb7
Kurt Garcia <akodevs@Kurts-Mac-mini.local>
2016-12-10 23:38:12 -0800

    removed fs package since fs is already in node package

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,13 @@ var path = require('path');
 var fileStream = require('fs');
 var browserSync = require('browser-sync').create();
 
-
+var plugins = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*'],
+    rename: { 'event-stream': 'es' },
+    config: path.join(__dirname, 'package.json'),
+    lazy: false 
+});
+plugins.browserSync = browserSync;
+plugins.path = path;
 
 
